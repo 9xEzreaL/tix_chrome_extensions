4c61959729fe3536cdcf92586cb50e0e73eb76f8
9xEzreaL <goodmen5488@gmail.com>
2024-03-22 17:38:39 +0800

    Ibon first ver ok.

--- js/getticket_ibon.js
+++ js/getticket_ibon.js
@@ -3,37 +3,263 @@
 $(()=>{
     window.alert = function() { console.log('Alert blocked.'); };
 
-    function compareTime(){
-    // start time
-        chrome.storage.local.get([
-        "tixcraft_date",
-        "tixcraft_time",
-        ],(result) => {
-        var date1 = result.tixcraft_date + "T" + result.tixcraft_time;
-        const endDate = new Date(date1);
-        const startDate = new Date()
-        let difference = endDate.getTime() - startDate.getTime();
-        async function exampleFunction() {
-            console.log('Code before sleep');
-            await sleep(difference);
-            console.log('Code after sleep');
-        }
+    function sleep(milliseconds) {
+          var start = new Date().getTime();
+          while (true) {
+            if ((new Date().getTime() - start) > milliseconds){
+              break;
+            }
+          }
+        };
 
-        exampleFunction();
-        console.log(`The timeout is set for ${difference} milliseconds.`);
-    })}
+    function compareTime() {
+        return new Promise((resolve, reject) => {
+            chrome.storage.local.get(["tixcraft_date", "tixcraft_time"], (result) => {
+                var date1 = result.tixcraft_date + "T" + result.tixcraft_time;
+                const endDate = new Date(date1);
+                const startDate = new Date();
+                let difference = endDate.getTime() - startDate.getTime();
 
-    // 1 step -> 定時功能
-    compareTime()
-
-    if (window.location.href.indexOf("/ActivityInfo/Details/")>0){
-    // 2 step -> 進入立即購票選日期
-        chrome.storage.local.get([
-        "tixcraft_date_index", // index
-        "tixcraft_date", // 啟動日期
-        "tixcraft_time", // 啟動時間
-        ],(result) => {})
+                if (difference > 0) {
+                    setTimeout(() => {
+                        console.log('Time has arrived.');
+                        resolve(true);  // 時間到達
+                    }, difference);
+                    console.log(`The timeout is set for ${difference} milliseconds.`);
+                } else {
+                    console.log('The specified time has already passed.');
+                    resolve(false); // 時間已過
+                }
+            });
+        });
     }
 
+    function waitForElementToBeClickable(selector, timeout = 30000) {
+        return new Promise((resolve, reject) => {
+            const startTime = Date.now();
+
+            // 檢查元素是否可點擊
+            function checkIfClickable(el) {
+                // console.log("disable")
+                // console.log(el.disabled)
+                // console.log("display")
+                // console.log(getComputedStyle(el).display)
+                // console.log("style")
+                // console.log(getComputedStyle(el).visibility)
+                if (el && !el.disabled && getComputedStyle(el).display !== 'none' && getComputedStyle(el).visibility !== 'hidden') {
+                    resolve(el);
+                    console.log('clickable')
+                } else if (Date.now() - startTime > timeout) {
+                    reject(new Error('Element not clickable within timeout'));
+                } else {
+                    console.log('not clickable')
+                    setTimeout(checkIfClickable, 1000);  // 每100毫秒檢查一次
+
+                }
+            }
+
+            checkIfClickable(selector);
+        });
+    }
+
+    function waitForElement(selector, timeout = 30000) {
+        return new Promise((resolve, reject) => {
+            const startTime = Date.now();
+
+            function checkForElement() {
+                const element = document.querySelector(selector);
+                if (element) {
+                    resolve(element);
+                } else if (Date.now() - startTime > timeout) {
+                    reject(new Error('Element not found within the specified timeout'));
+                } else {
+                    setTimeout(checkForElement, 100); // Check again after a short delay
+                }
+            }
+
+            checkForElement();
+        });
+    }
+
+    // Chrome扩展中的JavaScript示例
+    function sendImageToOCR(imageBase64) {
+        console.log(imageBase64)
+        fetch('http://localhost:5000/ocr', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ image: imageBase64 })
+        })
+        .then(response => response.json())
+        .then(data => {
+            const inputElement = document.getElementById('ctl00_ContentPlaceHolder1_CHK');
+            if (inputElement) {
+                inputElement.value = data.text;  // 将OCR结果设置为input元素的值
+                let elBtn = document.querySelector('#ticket-wrap > a.btn');
+                if (elBtn && !elBtn.disabled) {
+                    elBtn.click();
+                }
+            }
+        })
+        .catch(error => console.error('Error:', error));
+    }
+
+    function getImage(imageId) {
+        let canvas = document.createElement('canvas');
+        let context = canvas.getContext('2d');
+        let img = document.getElementById(imageId); // Use the actual image ID
+        canvas.height = img.naturalHeight;
+        canvas.width = img.naturalWidth;
+        context.drawImage(img, 0, 0);
+        return canvas.toDataURL(); // Return the base64 string
+    }
+
+
+    // 1 step -> 定時功能
+    compareTime().then(timeArrived => {
+        if (window.location.href.indexOf("/ActivityInfo/Details/")>0){
+        // 2 step -> 進入立即購票選日期
+            chrome.storage.local.get([
+            "tixcraft_date_index", // index
+            ],(result) => {
+                let myCssSelector = "div.single-content > div > div.row > div > div.tr";
+
+                // Wait for the page to load
+                function findAndClick(selector, maxAttempts = 30, interval = 300) {
+                    let attempts = 0;
+
+                    console.log(`Searching for element with selector: ${selector}`);
+
+                    const intervalId = setInterval(() => {
+                        const element = document.querySelectorAll(selector);
+                        console.log(element)
+                        if (element.length != 0) {
+                            clearInterval(intervalId);
+                            try {
+                                element[result.tixcraft_date_index - 1].getElementsByTagName('button')[0].click()
+                            }
+                            catch (e) {
+                                element[0].getElementsByTagName('button')[0].click()
+                            }
+
+                            console.log('Element clicked:', element);
+                        } else {
+                            attempts++;
+                            console.log(`Attempt ${attempts}: Element not found`);
+
+                            if (attempts >= maxAttempts) {
+                                clearInterval(intervalId);
+                                console.log('Max attempts reached, reloading page...');
+                                window.location.reload();
+                            }
+                        }
+                    }, interval);
+                }
+                findAndClick(myCssSelector)
+            })
+        }
+
+        if (window.location.href.indexOf("PRODUCT_ID")>0){
+            // 3 step -> 選區域
+            let myCssSelector = 'div.col-md-5 > table > tbody > tr';
+
+            chrome.storage.local.get([
+            "tixcraft_area",
+            "tixcraft_area2",
+            "tixcraft_area3" // keyword
+            ],(result) => {
+                function findAreaClick(selector, maxAttempts = 30, interval = 300) {
+                    let attempts = 0;
+                    const intervalId = setInterval(() => {
+                        const elements = document.querySelectorAll(selector);
+                        console.log('elements', elements)
+                        if (elements.length != 0) {
+                            clearInterval(intervalId);
+                            try {
+                                elements[result.tixcraft_area - 1].click()
+                            }
+                            catch (e) {
+                                try {
+                                    elements[result.tixcraft_area2 - 1].click()
+                                }
+                                catch (e) {
+                                    try {
+                                        elements[result.tixcraft_area3 - 1].click()
+                                    }
+                                    catch (e) {
+                                        console.log("can not click elements in area.")
+                                    }
+                                }
+                            }
+                            // window.location.href = nextUrl;
+                        } else {
+                            attempts++;
+                            console.log(`Attempt ${attempts}: Element not found`);
+
+                            if (attempts >= maxAttempts) {
+                                clearInterval(intervalId);
+                                console.log('Max attempts reached, reloading page...');
+                                window.location.reload();
+                            }
+                        }
+                    }, interval)
+                }
+            findAreaClick(myCssSelector)
+            })
+        }
+
+        if (window.location.href.indexOf("PERFORMANCE_PRICE_AREA_ID")>0){
+            // 4 step -> 選張數 + 驗證碼
+            let myCssSelector = "ctl00_ContentPlaceHolder1_DataGrid_ctl02_AMOUNT_DDL";
+
+            chrome.storage.local.get([
+            "tixcraft_ticketcount" // Number
+            ],(result) => {
+                const ticketNumber = parseInt(result.tixcraft_ticketcount, 10);
+
+                // Now, use this ticketNumber in your script
+                function findTicketNumberClick(selector, maxAttempts = 30, interval = 300){
+                    let attempts = 0;
+                    const intervalId = setInterval(() => {
+                        const ticket = document.getElementById(selector);
+                        if (ticket.length != 0){
+                            clearInterval(intervalId);
+                            if (ticket && ticket.options && ticket.options[ticketNumber]) {
+                            ticket.value = ticket.options[ticketNumber].value;
+                            let imageBase64 = getImage('chk_pic');
+                            sendImageToOCR(imageBase64);
+                            }
+                        } else {
+                            attempts++;
+                            console.log(`Attempt ${attempts}: Element not found`);
+
+                            if (attempts >= maxAttempts) {
+                                clearInterval(intervalId);
+                                console.log('Max attempts reached, reloading page...');
+                                window.location.reload();
+                            }
+                        }
+                    })
+
+                // 預測ocr/ click final Btn
+                }
+                findTicketNumberClick(myCssSelector)
+
+            })
+
+
+
+        }
+
+    })
+
+    // 3 step -> 選區域
+    // https://orders.ibon.com.tw/application/UTK02/UTK0201_000.aspx?PERFORMANCE_ID=B06OLIRU&PRODUCT_ID=B06OEKX0&strItem=WEB%E7%B6%B2%E7%AB%99%E5%85%A5%E5%8F%A31
+    // https://orders.ibon.com.tw/application/UTK02/UTK0201_000.aspx?PERFORMANCE_ID=B06KDFWO&PRODUCT_ID=B06K8ZDQ&strItem=WEB%E7%B6%B2%E7%AB%99%E5%85%A5%E5%8F%A31
+
+
+    // 4 step -> 選張數
+    // https://orders.ibon.com.tw/application/UTK02/UTK0202_.aspx?PERFORMANCE_ID=B06KDFWO&GROUP_ID=&PERFORMANCE_PRICE_AREA_ID=B06KDGMC
 
 })
--- js/getticket_kktix.js
+++ js/getticket_kktix.js
@@ -12,136 +12,215 @@ $(()=>{
           }
         };
 
-    function compareTime(){
-        chrome.storage.local.get([
-            "tixcraft_date",
-            "tixcraft_time",
-            ],(result) => {
-            var date1 = result.tixcraft_date + "T" + result.tixcraft_time;
-            const endDate = new Date(date1);
-            const startDate = new Date()
-            let difference = endDate.getTime() - startDate.getTime();
-            async function exampleFunction() {
-                console.log('Code before sleep');
-                await sleep(difference);
-                console.log('Code after sleep');
-            }
+    function compareTime() {
+        return new Promise((resolve, reject) => {
+            chrome.storage.local.get(["tixcraft_date", "tixcraft_time"], (result) => {
+                var date1 = result.tixcraft_date + "T" + result.tixcraft_time;
+                const endDate = new Date(date1);
+                const startDate = new Date();
+                let difference = endDate.getTime() - startDate.getTime();
 
-            exampleFunction();
-            console.log(`The timeout is set for ${difference} milliseconds.`);
-        })}
-
-    compareTime()
-
-    if(document.querySelectorAll("input[type='text']").length == 1){
-        // if haven't selling -> reload
-        console.log("reload.");
-        sleep(500);
-        window.location.reload(true);
+                if (difference > 0) {
+                    setTimeout(() => {
+                        console.log('Time has arrived.');
+                        resolve(true);  // 時間到達
+                    }, difference);
+                    console.log(`The timeout is set for ${difference} milliseconds.`);
+                } else {
+                    console.log('The specified time has already passed.');
+                    resolve(true); // 時間已過
+                }
+            });
+        });
     }
-    else if(document.getElementById("person_agree_terms")!=null){
-        // sell -> click agree btn
-        document.getElementById("person_agree_terms").click();
-        // select area
-        chrome.storage.local.get([
-            "tixcraft_area",
-            "tixcraft_area2",
-            "tixcraft_area3",
-            "tixcraft_ticketcount",
-            ],(result)=> {
-            // ng-untouched ng-valid ng-not-empty ng-dirty ng-valid-parse
-        var elements = Array.from(document.querySelectorAll( "input[type='text']"));
-        var assignTicket = false;
-        try {
-            elements[result.tixcraft_area].value = result.tixcraft_ticketcount;
-            elements[result.tixcraft_area].dispatchEvent(new Event('change'));
-            assignTicket = true;
-        } catch (e) {
-            try {elements[result.tixcraft_area2].value = result.tixcraft_ticketcount;
-                elements[result.tixcraft_area2].dispatchEvent(new Event('change'));
-                assignTicket = true;
-            } catch (e) {
-                try {elements[result.tixcraft_area3].value = result.tixcraft_ticketcount;
-                    elements[result.tixcraft_area3].dispatchEvent(new Event('change'));
-                    assignTicket = true;
-                } catch (e) {
-                    console.log("no tickets");
-                    }
-            }
-        }
-        if (assignTicket){
-            // btn btn-primary btn-lg ng-isolate-scope btn-disabled-alt 不能點的
-            // btn btn-primary btn-lg ng-isolate-scope 可以點的
-            // console.log(document.getElementsByClassName("btn btn-primary btn-lg ng-isolate-scope"))
-            var checkBtn = document.getElementsByClassName("btn btn-primary btn-lg ng-isolate-scope")[0];
-            var intervalId = setInterval(function() {
-            // Check if the button is not disabled
-            if (!checkBtn.disabled) {
-                // Click the button
-                checkBtn.click();
-                console.log('Button clicked');
 
-                // Stop checking
+    // compareTime().then(timeArrived => {
+    //     if (timeArrived) {
+    //         console.log('Time has arrived, proceed with the operation.');
+    //         if(document.querySelectorAll("input[type='text']").length == 1){
+    //             // if haven't selling -> reload
+    //             console.log("reload.");
+    //             sleep(500);
+    //             window.location.reload(true);
+    //         }
+    //         else if(document.getElementById("person_agree_terms")!=null){
+    //             // sell -> click agree btn
+    //             document.getElementById("person_agree_terms").click();
+    //             // select area
+    //             chrome.storage.local.get([
+    //                 "tixcraft_area",
+    //                 "tixcraft_area2",
+    //                 "tixcraft_area3",
+    //                 "tixcraft_ticketcount",
+    //                 ],(result)=> {
+    //                 // ng-untouched ng-valid ng-not-empty ng-dirty ng-valid-parse
+    //             var elements = Array.from(document.querySelectorAll( "input[type='text']"));
+    //             var assignTicket = false;
+    //             try {
+    //                 elements[result.tixcraft_area].value = result.tixcraft_ticketcount;
+    //                 elements[result.tixcraft_area].dispatchEvent(new Event('change'));
+    //                 assignTicket = true;
+    //             } catch (e) {
+    //                 try {elements[result.tixcraft_area2].value = result.tixcraft_ticketcount;
+    //                     elements[result.tixcraft_area2].dispatchEvent(new Event('change'));
+    //                     assignTicket = true;
+    //                 } catch (e) {
+    //                     try {elements[result.tixcraft_area3].value = result.tixcraft_ticketcount;
+    //                         elements[result.tixcraft_area3].dispatchEvent(new Event('change'));
+    //                         assignTicket = true;
+    //                     } catch (e) {
+    //                         console.log("no tickets");
+    //                         }
+    //                 }
+    //             }
+    //             if (assignTicket){
+    //                 // btn btn-primary btn-lg ng-isolate-scope btn-disabled-alt 不能點的
+    //                 // btn btn-primary btn-lg ng-isolate-scope 可以點的
+    //                 // console.log(document.getElementsByClassName("btn btn-primary btn-lg ng-isolate-scope"))
+    //                 var checkBtn = document.getElementsByClassName("btn btn-primary btn-lg ng-isolate-scope")[0];
+    //                 var intervalId = setInterval(function() {
+    //                 // Check if the button is not disabled
+    //                 if (!checkBtn.disabled) {
+    //                     // Click the button
+    //                     checkBtn.click();
+    //                     console.log('Button clicked');
+    //
+    //                     // Stop checking
+    //                     clearInterval(intervalId);
+    //                 } else {
+    //                     console.log('Button not clickable yet');
+    //                 }
+    //             }, 500); // Check every 1000 milliseconds (1 second)
+    //                 // checkBtn.click();
+    //                 // checkBtn.dispatchEvent(new Event('change'));
+    //                 console.log("assign ticket already");
+    //             }
+    //             else{
+    //                 // area ticket can not assign
+    //                 window.location.reload(true);}
+    //                 }
+    //             )
+    //         }
+    //         else{window.location.reload(true);}
+    //     } else {
+    //         console.log('Time has already passed, handle accordingly.');
+    //     }
+    // });
+
+function clickBtn(){
+    // btn btn-primary btn-lg ng-isolate-scope btn-disabled-alt 不能點的
+    // btn btn-primary btn-lg ng-isolate-scope 可以點的
+    // console.log(document.getElementsByClassName("btn btn-primary btn-lg ng-isolate-scope"))
+    var checkBtn = document.getElementsByClassName("btn btn-primary btn-lg ng-isolate-scope")[0];
+    console.log("get check btn")
+    var intervalId = setInterval(function() {
+    // Check if the button is not disabled
+        console.log(checkBtn.disabled)
+        if (!checkBtn.disabled) {
+        // Click the button
+        checkBtn.click();
+        console.log('Button clicked');
+
+        // Stop checking
+        clearInterval(intervalId);
+    } else {
+        console.log('Button not clickable yet');
+    }
+}, 500); // Check every 1000 milliseconds (1 second)
+    // checkBtn.click();
+    // checkBtn.dispatchEvent(new Event('change'));
+    console.log("assign ticket already");
+
+}
+
+function findArea(maxAttempts = 5, interval = 500, callback) {
+    let attempts = 0;
+
+    console.log('Searching for elements with input type text');
+
+    const intervalId = setInterval(() => {
+        const elements = Array.from(document.querySelectorAll("input[type='text']"));
+
+        if (elements.length > 0) {
+            console.log('Element found:', elements);
+            clearInterval(intervalId);
+            if (callback) {
+                callback(elements);  // Pass the elements to the callback function
+            }
+        } else {
+            attempts++;
+            console.log(`Attempt ${attempts}: Element not found`);
+
+            if (attempts >= maxAttempts) {
                 clearInterval(intervalId);
-            } else {
-                console.log('Button not clickable yet');
+                console.log('Max attempts reached, reloading page...');
+                // window.location.reload();
             }
-        }, 500); // Check every 1000 milliseconds (1 second)
-            // checkBtn.click();
-            // checkBtn.dispatchEvent(new Event('change'));
-            console.log("assign ticket already");
         }
-        else{
-            // area ticket can not assign
-            window.location.reload(true);}
+    }, interval);
+}
+
+var assignTicket = false;
+function handleTimeCheck() {
+    compareTime().then(timeArrived => {
+        if (timeArrived) {
+            console.log('Time has arrived, proceed with the operation.');
+
+            if(document.getElementById("person_agree_terms")!=null){
+                // sell -> click agree btn
+
+                document.getElementById("person_agree_terms").click();
+                // select area
+                chrome.storage.local.get([
+                    "tixcraft_area",
+                    "tixcraft_area2",
+                    "tixcraft_area3",
+                    "tixcraft_ticketcount",
+                    ],(result)=> {
+                    // ng-untouched ng-valid ng-not-empty ng-dirty ng-valid-parse
+                // var elements = Array.from(document.querySelectorAll( "input[type='text']"));
+
+                window.addEventListener('load', () => {
+                    findArea(5, 500, (elements) => {
+                        try {
+                            elements[result.tixcraft_area].value = result.tixcraft_ticketcount;
+                            elements[result.tixcraft_area].dispatchEvent(new Event('change'));
+                            clickBtn()
+                            assignTicket = true;
+                        } catch (e) {
+                            try {
+                                elements[result.tixcraft_area2].value = result.tixcraft_ticketcount;
+                                elements[result.tixcraft_area2].dispatchEvent(new Event('change'));
+                                clickBtn()
+                                assignTicket = true;
+                            } catch (e) {
+                                try {
+                                    elements[result.tixcraft_area3].value = result.tixcraft_ticketcount;
+                                    elements[result.tixcraft_area3].dispatchEvent(new Event('change'));
+                                    clickBtn()
+                                    assignTicket = true;
+                                } catch (e) {
+                                    console.log("no tickets");
+                                }
+                            }
+                        }
+                    })
+                })
+
+                    }
+                )
             }
-        )
-    }
-    else{window.location.reload(true);}
+            else{window.location.reload(true);}
+            //
+        } else {
+            console.log('Time has not arrived yet, checking again...');
+            setTimeout(handleTimeCheck, 1000); // 等待1秒後重新檢查
+        }
+    });
+}
 
-//         //kktix
-//  chrome.storage.local.get([
-//     "tms_using",
-//     "tms_EventType",
-//     "tms_GovernmentType",
-//     "tms_SellTickets",
-//     "tms_date",
-//     "tms_start",
-//     "tms_SubVenuesTagDiv",
-//     "tms_end",
-//     "tms_Broadcast",
-//     "tms_Billboards",
-//     "tms_Stalls",
-//     "tms_EventName",
-//     "tms_EventDescription",
-//     "tms_EventSportType",
-//     "tms_EventParticipantsNumber"
-//   ],(result)=> {
-//
-//
-//
-// let getticket=$(".ticket-unit.ng-scope").filter(key => $($(".ticket-unit.ng-scope")[key]).find(".ticket-price").text().includes("3,800"));
-// getticket.find("input").val("1")
-// window.alert = function(str){
-// return ;
-// }
-// alert("不能彈出警示框");//-->
-// $(".btn.btn-lg.ng-isolate-scope.btn-primary")
-//         document.getElementById("EventType").value=result.tms_EventType;
-//         document.getElementById("GovernmentType").value=result.tms_GovernmentType;
-//         document.getElementById("SellTickets").value=result.tms_SellTickets
-//
-//
-//   });
-//
-//     }
-//     window.alert = function(str){
-//     return ;
-//     }
-//     alert("不能彈出警示框")
 
+handleTimeCheck();  // 開始時間檢查流程
 })
-   
-
        
       
